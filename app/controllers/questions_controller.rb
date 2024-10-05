697b208817f68c8ca32c9452bea691598be5ad7e
samy boursiquot <samyboursiquot4@gmail.com>
2024-10-05 15:37:32 -0400

    rails new

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,23 +1,22 @@
 class QuestionsController < ApplicationController
   def ask
-    #
-  end
-  def
-    @question = fetch_question
-    @answer = answer(@question)
   end
 
-private
+  def answer
+    @question = params[:question]
+    @answer = coach_answer(@question)
+  end
 
-def fetch_question
-  params[:question]
-end
+  private
 
-def answer(question)
-  if question.downcase == "j'ai une question"
-  elsif question.end_with?("?")
-    "Silly question, get dressed and go to work!"
-  else
-    "I don't care, get dressed and go to work!"
+  def coach_answer(question)
+    case question.downcase
+    when "i am going to work"
+      "Great!"
+    when ->(q) { q.end_with?("?") }
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # afficher le formulaire
+  root "questions#ask" # page principale
   get "ask", to: "questions#ask"
 
   # afficher la reponse
